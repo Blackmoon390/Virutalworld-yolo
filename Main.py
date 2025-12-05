356a60e3dfab2163b0d9b1dc26d00a1b6b107ac2
Blackmoon390 <amsama1134@gmail.com>
2025-12-05 13:52:36 +0530

    Add files via upload

--- Main.py
+++ Main.py
@@ -13,34 +13,7 @@ model=YOLO("yolo11n-seg.pt")
 bluecolour=np.full((640,480,3),(24, 34, 200),dtype='uint8')
 bluecolour=cv2.cvtColor(bluecolour,cv2.COLOR_BGR2RGB)
 
-def apply_blue_black_noise(frame, mask, block=2):
-    h, w = frame.shape[:2]   # frame = (640, 480, 3)
 
-    # --- Fix mask shape mismatch ----
-    if mask.shape != (h, w):
-        mask = cv2.resize(mask, (w, h))
-
-    # Make binary mask
-    mask = (mask > 0).astype(np.uint8) * 255
-
-    # Convert mask to 3-channel
-    mask3 = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
-
-    # Noise generation
-    small_h = h // block
-    small_w = w // block
-
-    rnd = np.random.randint(0, 2, (small_h, small_w), dtype=np.uint8)
-
-    noise_small = np.zeros((small_h, small_w, 3), dtype=np.uint8)
-    noise_small[rnd == 1] = frame[0, 0]
-
-    big_noise = cv2.resize(noise_small, (w, h), interpolation=cv2.INTER_NEAREST)
-
-    # Apply mask (only noise inside)
-    inside = cv2.bitwise_and(big_noise, mask3)
-
-    return inside
 
 
 cam=cv2.VideoCapture(0)
@@ -56,16 +29,15 @@ while cam.isOpened():
             
             for cls,data in zip(r.names,r.masks.data):
                 if int(cls) == 0:
+                   global mask
                    mask = data.cpu().numpy().astype('uint8')*255
                    mask=cv2.resize(mask,(640,480))
     mask_not=cv2.bitwise_not(mask)
  
     print(mask.shape,bluecolour.shape)
-    person_mask_colour=apply_blue_black_noise(bluecolour, mask)
+    # person_mask_colour=tpm.apply_blue_black_noise(bluecolour, mask)
 
     cv2.imshow('mask',person_mask_colour)       
-
-
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
